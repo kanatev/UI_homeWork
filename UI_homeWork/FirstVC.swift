0d32ee9e8a663317329ddeea347e4ed123f66248
Aleksei Kanatev <aleksey.kanatev@mail.ru>
2019-06-30 10:16:35 +0500

    Added: FirstVC with elements and constraints

---
+++ UI_homeWork/FirstVC.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UI_homeWork
+//
+//  Created by Aleksei Kanatev on 30/06/2019.
+//  Copyright © 2019 Aleksei Kanatev. All rights reserved.
+//
+
+import UIKit
+
+class FirstVC: UIViewController {
+
+    @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
