7895f5efc5d7f875e3e3c995a2715c5fc1aab232
Aleksei Kanatev <aleksey.kanatev@mail.ru>
2019-06-30 11:58:46 +0500

    Added: data verification, white status bar

--- UI_homeWork/FirstVC.swift
+++ UI_homeWork/FirstVC.swift
@@ -10,13 +10,48 @@ import UIKit
 
 class FirstVC: UIViewController {
 
+    // white status bar
+    var barStyle = UIStatusBarStyle.lightContent
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return barStyle
+    }
+    
     @IBOutlet var loginTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var enterButtonOutlet: UIButton!
     
+    func clearTextFields() {
+        loginTextField.text = nil
+        passwordTextField.text = nil
+    }
+    
+    @IBAction func enterButton(_ sender: Any) {
+
+        if !loginTextField.text!.isEmpty && !passwordTextField.text!.isEmpty {
+            let login = loginTextField.text!
+            let password = passwordTextField.text!
+            print("Entered data: Login: \(login), Password: \(password)")
+            
+            if login == "admin" && password == "123456" {
+                print("access granted\n")
+                clearTextFields()
+            } else {
+                print("access denied\n")
+                clearTextFields()
+            }
+            
+        } else {
+            print("Операция не может быть выполнена. Заполнены не все поля.\n")
+            clearTextFields()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        loginTextField.placeholder = "Email или телефон"
+        passwordTextField.placeholder = "Пароль"
+        enterButtonOutlet.layer.cornerRadius = 5
     }
 
 
