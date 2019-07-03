2196c368c8bc557aa9d607301260a2a03bc8621d
Aleksei Kanatev <aleksey.kanatev@mail.ru>
2019-07-03 15:50:00 +0500

    Added: array with friends and exit buttons

--- UI_homeWork/Controller/FirstVC.swift
+++ UI_homeWork/Controller/FirstVC.swift
@@ -10,8 +10,10 @@
 
 import UIKit
 
-class FirstVC: UIViewController {
-
+class FirstVC: UIViewController, UIAlertViewDelegate {
+    
+    
+    
     // white status bar
     var barStyle = UIStatusBarStyle.lightContent
     override var preferredStatusBarStyle: UIStatusBarStyle{
@@ -22,41 +24,37 @@ class FirstVC: UIViewController {
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var enterButtonOutlet: UIButton!
     
+    
     func clearTextFields() {
         loginTextField.text = nil
         passwordTextField.text = nil
     }
     
     @IBAction func enterButton(_ sender: Any) {
-
-        if !loginTextField.text!.isEmpty && !passwordTextField.text!.isEmpty {
-            let login = loginTextField.text!
-            let password = passwordTextField.text!
-            print("Entered data: Login: \(login), Password: \(password)")
-            
-            if login == "admin" && password == "123456" {
-                print("access granted\n")
-                clearTextFields()
-            } else {
-                print("access denied\n")
-                clearTextFields()
-            }
-            
-        } else {
-            print("Операция не может быть выполнена. Заполнены не все поля.\n")
-            clearTextFields()
-        }
+    }
+    
+    func showArert() {
+        let alert: UIAlertController = UIAlertController(title: "Ошибка", message: "Неправильный логин или пароль", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        clearTextFields()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         loginTextField.placeholder = "Email или телефон"
         passwordTextField.placeholder = "Пароль"
         enterButtonOutlet.layer.cornerRadius = 5
     }
-
-
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if loginTextField.text == "admin" && passwordTextField.text == "123456" {
+            clearTextFields()
+            return true
+        } else {
+            showArert()
+            return false
+        }
+    }
 }
-
-// Отправляю на проверку
