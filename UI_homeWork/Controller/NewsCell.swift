e8ce892c2ec4e12fbb44e3959dd89f427a758e0f
Aleksei Kanatev <aleksey.kanatev@mail.ru>
2020-05-03 18:51:13 +0300

    добавил кликабельные лайки в новости

--- UI_homeWork/Controller/NewsCell.swift
+++ UI_homeWork/Controller/NewsCell.swift
@@ -8,17 +8,54 @@
 
 import UIKit
 
+protocol NewsCellDelegate: class {
+    func heartTapped(at indexPath: IndexPath)
+}
+
 class NewsCell: UITableViewCell {
 
+    @IBOutlet weak var heartImageView: UIImageView!
+    @IBOutlet weak var heartCounterLabel: UILabel!
     @IBOutlet weak var nameForNews: UILabel!
     @IBOutlet weak var textOfNews: UILabel!
     @IBOutlet weak var imageForNews: UIImageView!
     
+//    public var heartState = false
+    
+    public var heartFilled = false {
+        didSet {
+            if !heartFilled {
+                heartImageView.image = UIImage(systemName: "heart")
+            } else {
+                heartImageView.image = UIImage(systemName: "heart.fill")
+            }
+        }
+    }
+    
+    public weak var delegate: NewsCellDelegate?
+    public var heartPressed = {}
+    public var cellIndexPath = IndexPath(item: 0, section: 0)
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
+        heartImageView.image = UIImage(systemName: "heart")
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(heartTapped))
+        heartImageView.addGestureRecognizer(tapGestureRecognizer)
     }
-
+    
+    @objc private func heartTapped(){
+        heartFilled.toggle()
+        heartPressed()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        heartImageView.image = UIImage(systemName: "heart")
+    }
+    
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
