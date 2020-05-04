1ae62e0708ca862df30016840421df2607903da7
Aleksei Kanatev <aleksey.kanatev@mail.ru>
2020-05-04 15:36:46 +0300

    отключил выделение ячеек при нажатии

--- UI_homeWork/Controller/XibCell.swift
+++ UI_homeWork/Controller/XibCell.swift
@@ -15,11 +15,12 @@ class XibCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        self.selectionStyle = .none
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
     }
     
 }
